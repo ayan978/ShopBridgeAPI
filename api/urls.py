957198ebf0f77b6cea27715761dcf48b27d37510
unknown <sadiqunnur.97@gmail.com>
2024-10-13 20:40:09 +0600

    urls implemented

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 
 
 
@@ -9,8 +10,19 @@ router = DefaultRouter()
 
 router.register("products", views.ProductsViewSet)
 router.register("categories", views.CategoryViewSet)
+router.register("carts", views.CartViewSet)
+
+
+
+product_router = NestedDefaultRouter(router, "products", lookup="product")
+product_router.register("reviews", views.ReviewViewSet, basename="product-reviews")
+
+cart_router = NestedDefaultRouter(router, "carts", lookup="cart")
+cart_router.register("items", views.CartItemViewSet, basename="cart-items")
 
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(product_router.urls)),
+    path("", include(cart_router.urls))
 ]
