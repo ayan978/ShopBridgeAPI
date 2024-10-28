b908eaf8811476e3837a982fe8c6bf31853bbe6d
unknown <sadiqunnur.97@gmail.com>
2024-10-29 02:03:09 +0600

    cart item functionalities added

--- api/views.py
+++ api/views.py
@@ -46,10 +46,30 @@ class ReviewViewSet(ModelViewSet):
     
     def get_serializer_context(self):
         return {"product_id": self.kwargs["product_pk"]}
-8
         
             
 
 class CartViewSet(CreateModelMixin,RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+
+class CartItemViewSet(ModelViewSet):
+    
+    http_method_names = ["get", "post", "patch", "delete"]
+    
+    def get_queryset(self):
+        return Cartitems.objects.filter(cart_id=self.kwargs["cart_pk"])
+    
+    
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return AddCartItemSerializer
+        
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        
+        return CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {"cart_id": self.kwargs["cart_pk"]}
